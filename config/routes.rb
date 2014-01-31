31e62d514aeefea711d0b8087f01f14c74d732f1
Cedric Winbush <cawinbushjr@gmail.com>
2014-01-31 15:29:10 -0500

    Added Devise and Devise Invitable

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 PrivateInvite::Application.routes.draw do
+  devise_for :users
   root 'static_pages#home'
   match '/about', to: 'static_pages#about' ,via:'get'
   match '/contact', to: 'static_pages#contact', via:'get'
